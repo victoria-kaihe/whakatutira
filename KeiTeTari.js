25313f59f374f312eaf4482e179a1d54d1dc8934
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-08-07 11:02:44 +1200

    first commit

---
+++ KeiTeTari.js
@@ -0,0 +1,16 @@
+module.exports = [
+
+  {
+    kupuMaori: 'tari kāwanatanga',
+    kupuEnglish: 'public service',
+    contextMaori: 'Kei te tari kāwanatanga au e mahi ana.',
+    contextEnglish: 'I work in the public service.'
+  },
+  {
+    kupuMaori: 'ipurangi',
+    kupuEnglish: 'internet',
+    contextMaori: 'Kua tuwhera te ao Māori ki te ipurangi.',
+    contextEnglish: 'The Māori world has embraced the internet.'
+  }
+
+  ]
