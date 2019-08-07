a7cbf5b44a95350ed3bfdc35bf6956f45a450a52
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-08-07 15:29:34 +1200

    add more words

--- KeiTeTari.js
+++ KeiTeTari.js
@@ -13,10 +13,58 @@ module.exports = [
     contextEnglish: 'The Māori world has embraced the internet.'
   },
   {
-    kupuMāori: 'ipurangi',
-    kupuEnglish: 'internet',
-    contextMāori: 'Kua tuwhera te ao Māori ki te ipurangi.',
-    contextEnglish: 'The Māori world has embraced the internet.'
+    kupuMāori: 'rorohiko',
+    kupuEnglish: 'computer',
+    contextMāori: 'I hoko au he rorohiko hou.',
+    contextEnglish: 'I bought a new computer.'
+  },
+  {
+    kupuMāori: 'pūreretā',
+    kupuEnglish: 'printer',
+    contextMāori: 'Te āhua nei kua pau te pūreretā.',
+    contextEnglish: 'It looks like the printer has given up.'
+  },
+  {
+    kupuMāori: 'kānataraka',
+    kupuEnglish: 'contract',
+    contextMāori: 'Kua hainatia te kānataraka.',
+    contextEnglish: 'The contract has been signed.'
+  },
+  {
+    kupuMāori: 'rāngai tūmataiti',
+    kupuEnglish: 'private sector',
+    contextMāori: 'Kei te rāngai tūmataiti au e mahi ana.',
+    contextEnglish: 'I work in the private sector.'
+  },
+  {
+    kupuMāori: 'wāhi mahi',
+    kupuEnglish: 'workplace, work address',
+    contextMāori: 'Kei hea tō wāhi mahi?',
+    contextEnglish: 'What\'s your work address?'
+  },
+  {
+    kupuMāori: 'īmēra',
+    kupuEnglish: 'email',
+    contextMāori: 'Īmera mai mehemea he whakaaro ōu mō taua kaupapa.',
+    contextEnglish: 'Email me if you have any thoughts on the matter.'
+  },
+  {
+    kupuMāori: 'hui ā-ataata',
+    kupuEnglish: 'video conference',
+    contextMāori: 'Āhea te hui ā-ataata nei tīmata ai?',
+    contextEnglish: 'When does the video conference start?'
+  },
+  {
+    kupuMāori: 'wātaka',
+    kupuEnglish: 'calendar',
+    contextMāori: 'Ka kite au ki taku wātaka.',
+    contextEnglish: 'I\'ll take a look at my calendar.'
+  },
+  {
+    kupuMāori: '',
+    kupuEnglish: '',
+    contextMāori: '',
+    contextEnglish: ''
   }
 
   ]
