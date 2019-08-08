45a372494b84d12962849429126d86570fa0b19a
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-08-08 14:49:31 +1200

    add words to kainga list

--- KeiTeiWhare.js
+++ KeiTeiWhare.js
@@ -1,5 +1,59 @@
 module.exports = [
   {
+    kupuMāori: 'horoi',
+    kupuEnglish: 'wash',
+    contextMāori: 'Horoi tō ringaringa.',
+    contextEnglish: 'Wash your hands.'
+  },
+  {
+    kupuMāori: 'rūma noho',
+    kupuEnglish: 'living room',
+    contextMāori: 'Kua āhuru koe ki te rūma noho.',
+    contextEnglish: 'Make yourself comfortable in the living room.'
+  },
+  {
+    kupuMāori: 'mahi māra',
+    kupuEnglish: 'garden',
+    contextMāori: 'Kei te mahi ia ki te māra.',
+    contextEnglish: 'He is working in the garden.'
+  },
+  {
+    kupuMāori: 'ngote puehu',
+    kupuEnglish: 'vacuum',
+    contextMāori: '',
+    contextEnglish: ''
+  },
+  {
+    kupuMāori: 'peke e taea te whakamahi',
+    kupuEnglish: 'reusable bags',
+    contextMāori: 'Kaua e wareware nga peke e taea te whakamahi.',
+    contextEnglish: 'Don\'t forget the reusable bags.'
+  },
+  {
+    kupuMāori: 'kāuta',
+    kupuEnglish: 'kitchen',
+    contextMāori: '',
+    contextEnglish: ''
+  },
+  {
+    kupuMāori: 'pouaka whakaata',
+    kupuEnglish: 'television',
+    contextMāori: '',
+    contextEnglish: ''
+  },
+  {
+    kupuMāori: 'huarahi',
+    kupuEnglish: 'street',
+    contextMāori: '',
+    contextEnglish: ''
+  },
+  {
+    kupuMāori: 'ārai',
+    kupuEnglish: 'curtains',
+    contextMāori: 'Kati ngā ārai.',
+    contextEnglish: 'Close the curtains.'
+  },
+  {
     kupuMāori: '',
     kupuEnglish: '',
     contextMāori: '',
