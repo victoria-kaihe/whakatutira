cb3977d7964446dd88f6f5d529dc5a625cc131ed
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-08-08 14:00:25 +1200

    add more kīwaha

--- Kīwaha.js
+++ Kīwaha.js
@@ -10,26 +10,33 @@ module.exports = [
   {
     kīwahaMāori: 'Ko tāua tēnā?',
     kīwahaEnglish: 'Is that us?'
-  },{
+  },
+  {
     kīwahaMāori: 'Kei Mahurangi kē koe!',
     kīwahaEnglish: 'You\'re dreaming!'
-  },{
+  },
+  {
     kīwahaMāori: 'Mā hea mai ki tēnā.',
     kīwahaEnglish: 'It\'s the thought that counts.'
-  },{
-    kīwahaMāori: '',
-    kīwahaEnglish: ''
-  },{
-    kīwahaMāori: '',
-    kīwahaEnglish: ''
-  },{
-    kīwahaMāori: '',
-    kīwahaEnglish: ''
-  },{
-    kīwahaMāori: '',
-    kīwahaEnglish: ''
-  },{
-    kīwahaMāori: '',
-    kīwahaEnglish: ''
+  },
+  {
+    kīwahaMāori: 'Aheiha!',
+    kīwahaEnglish: 'Hard!'
+  },
+  {
+    kīwahaMāori: 'E kare hoki...',
+    kīwahaEnglish: 'Simmer down...'
+  },
+  {
+    kīwahaMāori: 'I waihotia atu ō taringa ki ti kainga?',
+    kīwahaEnglish: 'Did you leave your ears at home?'
+  },
+  {
+    kīwahaMāori: 'Ehara tonu!',
+    kīwahaEnglish: 'Surprise surprise!'
+  },
+  {
+    kīwahaMāori: 'Ki hea noa iho.',
+    kīwahaEnglish: 'You\'re tempting fate.'
   },
 ]
